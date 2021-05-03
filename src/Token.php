e2e7c26e0a084478c2cc3fbedef680c5a9b33917
César de la Cal Bretschneider <cesar@magic3w.com>
2021-05-03 11:19:42 +0200

    Audience and client
    Test Plan: Audience and client should be implemented
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D654

--- src/Token.php
+++ src/Token.php
@@ -1,7 +1,6 @@
 <?php namespace magic3w\phpauth\sdk;
 
-use Exception;
-use spitfire\io\request\Request;
+use \Lcobucci\JWT\Token\RegisteredClaims;
 
 class Token
 {
@@ -24,14 +23,7 @@ class Token
 	 */
 	private $expires;
 	
-	/**
-	 * 
-	 * @param SSO $sso
-	 * @param string $token
-	 * @param int $expires
-	 */
-	public function __construct(SSO $sso, string $token, int $expires)
-	{
+	public function __construct(SSO $sso, \Lcobucci\JWT\Token $token, int $expires) {
 		$this->sso = $sso;
 		$this->token = $token;
 		$this->expires = $expires;
@@ -58,5 +50,42 @@ class Token
 	
 	public function isAuthenticated() {
 		return $this->expires > time();
+	}
+	
+	/**
+	 * Returns the audience for this token. If the token is not issued to this application
+	 * we should fail it. This is generally the case because we will rarely use this SDK
+	 * to handle tokens that were not signed for us and have a different audience than us.
+	 * 
+	 * @return int The app id of the audience.
+	 */
+	public function audience() 
+	{
+		/**
+		 * We know that the token can only have one audience. So we use the first item of the
+		 * claims array.
+		 */
+		return reset($this->token->claims->get(RegisteredClaims::AUDIENCE, []));
+	}
+	
+	/**
+	 * This is the client the token was issued to. This means that, this application
+	 * is to be blamed for any changes or requests made using this token.
+	 * 
+	 * The application has been properly authenticated by using it's client credentials
+	 * to ensure this token was valid.
+	 * 
+	 * To do this we're using the non-standard claim `for`, which indicates that the token
+	 * is issued to be used by this application.
+	 * 
+	 * @return int The app id of the client attemptin to access the data
+	 */
+	public function client () 
+	{
+		/**
+		 * We know that the token can only have one issuing client. So we use the first item of the
+		 * claims array.
+		 */
+		return reset($this->token->claims->get(RegisteredClaims::ISSUER, []));
 	}
 }
