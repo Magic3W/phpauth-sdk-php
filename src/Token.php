80274b9c2e2657f3ddd5b024c189983b05248075
César de la Cal Bretschneider <cesar@magic3w.com>
2021-06-26 11:44:30 +0200

    Removed getTokenInfo method
    Test Plan: Just removed the code.
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D842

--- src/Token.php
+++ src/Token.php
@@ -47,22 +47,6 @@ class Token
 	}
 	
 	/**
-	 * 
-	 * @deprecated since v0.1.1 (20210331)
-	 * @return mixed
-	 */
-	public function getTokenInfo() {
-		static $cache = null;
-		
-		if ($cache !== null) { return $cache; }
-		
-		$request  = new Request($this->sso->getEndpoint() . '/auth/index/' . $this->token . '.json');
-		$response = $request->send()->expect(200)->json();
-		
-		return $cache = $response;
-	}
-	
-	/**
 	 * Indicates whether the session was expired, or whether the session is still active.
 	 * 
 	 * @return bool
